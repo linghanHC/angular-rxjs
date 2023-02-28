a9242b822bb6fd556eae93dcdaa004bf94a1cb48
linghanHC <37555834+linghanHC@users.noreply.github.com>
2023-02-28 11:03:51 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -14,8 +15,31 @@ import { bootstrapApplication } from '@angular/platform-browser';
     </a>
   `,
 })
-export class App {
-  name = 'Angular';
+export class App implements OnInit{
+  name = 'Angular'+VERSION.major;
+
+
+  ngOnInit(){
+    // of and from both generate observables, but they are different
+    const apples =['apple1','apple2'];
+    from(apples).subscribe(item=>console.log(item));   // apple1, apple2
+    of(apples).subscribe(item=>console.log(item));     // [apple1, apple2]
+    // ... js spreader function
+    of(...apples).subscribe(item=>console.log(item));  //apple1 apple2
+
+
+    console.log("============");
+    of (2,4,6).subscribe(item=>console.log(item));
+
+    from([10,20,30]).subscribe(
+      {
+        next: (item) => console.log(`Result: ${item}`),
+        error: (err) => console.log(`error occured..${err}`),
+        complete:() => console.log('complete'),
+      }
+    )
+  }
+
 }
 
 bootstrapApplication(App);
