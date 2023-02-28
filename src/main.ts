3c41a4fd4c514a819d9549ea90859046998cbfe2
linghanHC <37555834+linghanHC@users.noreply.github.com>
2023-02-28 11:34:39 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -20,39 +20,39 @@ export class App implements OnInit{
 
 
   ngOnInit(){
-    // console.log("======create function for observables======");
+    console.log("======create function for observables======");
 
-    // // of and from both create observables, but they are different
-    // const apples =['apple1','apple2'];
-    // from(apples).subscribe(item=>console.log(item));   // apple1, apple2
-    // of(apples).subscribe(item=>console.log(item));     // [apple1, apple2]
-    // // ... js spreader function
-    // of(...apples).subscribe(item=>console.log(item));  //apple1 apple2
+    // of and from both create observables, but they are different
+    const apples =['apple1','apple2'];
+    from(apples).subscribe(item=>console.log(item));   // apple1, apple2
+    of(apples).subscribe(item=>console.log(item));     // [apple1, apple2]
+    // ... js spreader function
+    of(...apples).subscribe(item=>console.log(item));  //apple1 apple2
 
-  //   console.log("=====rxjs operators=======");
+    console.log("=====rxjs operators=======");
 
-  //   of (1,2,3).pipe(
-  //     map(item=>item*2),    // transforms each emitted item
-  //     tap(item=>console.log("*", item)),  //tap into the emissions without affecting the items, used for debugging or performing actions outside of the flow of data (side effects, eg change the counter, )
-  //     take(2) // emits a specified number of items
-  //   ).subscribe(x=>console.log(x));
+    of (1,2,3).pipe(
+      map(item=>item*2),    // transforms each emitted item
+      tap(item=>console.log("*", item)),  //tap into the emissions without affecting the items, used for debugging or performing actions outside of the flow of data (side effects, eg change the counter, )
+      take(2) // emits a specified number of items
+    ).subscribe(x=>console.log(x));
 
-  //   from([10,20,30]).pipe(
-  //     tap(item=>console.log(`emitted item..${item}`)),
-  //     map(item=>item*2+10),
-  //     map(item=>{
-  //       if(item>50){
-  //         throw new Error('large value detected');
-  //       }
-  //       return item;
-  //     })
-  //   ).subscribe(
-  //     {
-  //       next: (x) => console.log(`Result: ${x}`),
-  //       error: (err) => console.log(`error occured..${err}`),
-  //       complete:() => console.log('complete'),
-  //     }
-  //   )
+    from([10,20,30]).pipe(
+      tap(item=>console.log(`emitted item..${item}`)),
+      map(item=>item*2+10),
+      map(item=>{
+        if(item>50){
+          throw new Error('large value detected');
+        }
+        return item;
+      })
+    ).subscribe(
+      {
+        next: (x) => console.log(`Result: ${x}`),
+        error: (err) => console.log(`error occured..${err}`),
+        complete:() => console.log('complete'),
+      }
+    )
   }
 
 }
